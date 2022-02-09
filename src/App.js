0346bdca3f825ac8ffa62c19d819c51ce9c49165
Abel Cabeza Román <contacto@abelcabezaroman.com>
2022-02-09 12:59:38 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import './App.css';
 import 'swiper/css';
-import { Navigation, Pagination } from "swiper";
 import SlideNextButton from "./components/SlideNextButton";
 
 
@@ -10,14 +9,8 @@ function App () {
     return (
         <div>
             <Swiper
-                modules={[Navigation, Pagination]}
                 spaceBetween={50}
                 slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
             >
                 <SwiperSlide>Slide 1</SwiperSlide>
                 <SwiperSlide>Slide 2</SwiperSlide>
